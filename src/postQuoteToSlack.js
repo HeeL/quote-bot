60a13402840e7a0df91760ac0978cb90b529a4f2
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-07-28 10:25:38 +0200

    Console logs and errors

--- src/postQuoteToSlack.js
+++ src/postQuoteToSlack.js
@@ -5,5 +5,8 @@ module.exports = ({slackClient, quotes}) => {
         channel: CHANNEL_ID_WHERE_TO_POST_QUOTE,
         text: `${quotes[0]} :squirell:`
     };
-    slack.chat.postMessage(postMessage);
+
+    slackClient.chat.postMessage(postMessageParams)
+        .then(console.log)
+        .catch(console.error);
 };
