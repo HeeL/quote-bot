a56e9af8c1844c0faa13688b5bedcbfb99165782
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-07-28 22:36:20 +0200

    Inject findQuoteOfTheDay function in index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Slack = require('slack');
 const postQuoteToSlack = require('./src/postQuoteToSlack');
+const findQuoteOfTheDay = require('./src/findQuoteOfTheDay');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,8 +11,8 @@ postQuoteToSlack({
         log: console.log,
         error: console.error
     },
+    findQuoteOfTheDay,
     config: {
         channelIdWhereToPostQuote: process.env.CHANNEL_ID_WHERE_TO_POST_QUOTE
-    },
-    quotes
+    }
 });
