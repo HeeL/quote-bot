40428087ee45130dcd26a29dfc37f7a20d611134
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-07-29 11:57:22 +0200

    Refactored function for finding dates difference

--- src/findQuoteOfTheDay.js
+++ src/findQuoteOfTheDay.js
@@ -1,10 +1,9 @@
 const Maybe = require('folktale/maybe');
 
 const dayOfTheYear = currentDate => {
-    const date1 = new Date(currentDate.getFullYear(), 0, 1, 12, 0, 0); // noon on Jan. 1
-    const date2 = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 12, 0, 0);
+    const januaryFirstThisYear = new Date(`${currentDate.getFullYear()}-01-01`);
 
-    return Math.round((date2 - date1) / 864e5) + 1;
+    return Math.round((currentDate - januaryFirstThisYear) / 864e5) + 1;
 };
 
 module.exports = (currentDate, quotes) => {
