c94faf9353c019dcf08245961cd2c357866e1620
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-10-04 23:34:26 +0200

    Use custom json data for quotes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Slack = require('slack');
 const postQuoteToSlack = require('./src/postQuoteToSlack');
 const findQuoteOfTheDay = require('./src/findQuoteOfTheDay');
 const dotenv = require('dotenv');
-
 dotenv.config();
+const quotes = require(`./src/quotes/${process.env.JSON_FILENAME_WITH_QUOTES}.json`);
 
 postQuoteToSlack({
     slackClient: new Slack({token: process.env.SLACK_TOKEN}),
@@ -12,6 +12,7 @@ postQuoteToSlack({
         error: console.error
     },
     findQuoteOfTheDay,
+    quotes,
     config: {
         channelIdWhereToPostQuote: process.env.CHANNEL_ID_WHERE_TO_POST_QUOTE
     }
