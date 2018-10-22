4ab5a5efff5a5fbfcc82d2d6e5bccf8d8eca612e
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-10-22 23:53:04 +0200

    Refator findQuoteOfTheDay() and test descriptions

--- src/findQuoteOfTheDay.js
+++ src/findQuoteOfTheDay.js
@@ -1,13 +1,13 @@
 const Maybe = require('folktale/maybe');
 
-const dayOfTheYear = currentDate => {
-    const januaryFirstThisYear = new Date(`${currentDate.getFullYear()}-01-01`);
+const getDayOfTheYear = currentDate => {
+    const firstDayOfTheYear = new Date(`${currentDate.getFullYear()}-01-01`);
 
-    return Math.round((currentDate - januaryFirstThisYear) / 864e5) + 1;
+    return Math.round((currentDate - firstDayOfTheYear) / 864e5) + 1;
 };
 
 module.exports = (currentDate, quotes) => {
-    const quoteOfTheDayIndex = (dayOfTheYear(currentDate) - 1) % quotes.length;
+    const quoteOfTheDayIndex = (getDayOfTheYear(currentDate) - 1) % quotes.length;
 
     return Maybe.fromNullable(quotes[quoteOfTheDayIndex]);
 };
--- test/unit/src/findQuoteOfTheDaySpec.js
+++ test/unit/src/findQuoteOfTheDaySpec.js
@@ -2,6 +2,8 @@ import test from 'ava';
 import Maybe from 'folktale/maybe';
 import findQuoteOfTheDay from '../../../src/findQuoteOfTheDay';
 
+const generateQuotes = (amount = 99) => Array.from({length: amount}, (_, i) => `Quote #${(i + 1).toString()}`);
+
 test('return Nothing if empty list provided', t => {
     const quotes = [];
     const quote = findQuoteOfTheDay(new Date(), quotes);
@@ -23,16 +25,16 @@ test('return the second quote from the list on the second day of the year', t =>
     t.deepEqual(quote, Maybe.Just('bar'));
 });
 
-test('return the 32nd quote on the first of february', t => {
-    const quotes = Array.from({length: 33}, (_, i) => `Quote #${(i + 1).toString()}`);
-    const quote = findQuoteOfTheDay(new Date('2018-02-01'), quotes);
+test('should take a quote based on day of the year', t => {
+    const the32ndDayOfTheYear = new Date('2018-02-01');
+    const quote = findQuoteOfTheDay(the32ndDayOfTheYear, generateQuotes());
 
     t.deepEqual(quote, Maybe.Just('Quote #32'));
 });
 
-test('return 4th quote on 12th of January if list of quotes has 8 items', t => {
-    const quotes = Array.from({length: 8}, (_, i) => `Quote #${(i + 1).toString()}`);
-    const quote = findQuoteOfTheDay(new Date('2018-01-12'), quotes);
+test('when current day is bigger than amount of quotes it should start from the beginning', t => {
+    const the12thDayOfTheYear = new Date('2018-01-12');
+    const quote = findQuoteOfTheDay(the12thDayOfTheYear, generateQuotes(8));
 
     t.deepEqual(quote, Maybe.Just('Quote #4'));
 });
